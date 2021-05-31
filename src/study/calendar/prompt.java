49fe1ff0d260286667920c4214ea0123665d07d2
NJ <NJ@DESKTOP-AS45D2C.lgcns.com>
2021-05-31 17:47:44 +0900

    update

--- src/study/calendar/prompt.java
+++ src/study/calendar/prompt.java
@@ -18,8 +18,13 @@ public class prompt {
 		//int repeat = sc.nextInt();
 		
 		int month = 1;
+		int year = -1;
 	
 		while (true) {
+			System.out.println("원하는 년을 입력하세요 : ");
+			System.out.print(PROMPT);
+			year = sc.nextInt();
+			
 			System.out.println("원하는 월을 입력하세요 : ");
 			System.out.print(PROMPT);
 			month = sc.nextInt();
@@ -29,9 +34,9 @@ public class prompt {
 			if (month > 12) {
 				continue;
 			}
-			System.out.printf("%d 월은 %d 일 까지 있습니다!\n", month, cal.getMaxDaysOfMonth(month));
+			System.out.printf("%d 월은 %d 일 까지 있습니다!\n", month, cal.getMaxDaysOfMonth(year,month));
 			
-			cal.printCalendar(2017, month);
+			cal.printCalendar(year, month);
 		}
 		
 		System.out.println("Bye~");		
